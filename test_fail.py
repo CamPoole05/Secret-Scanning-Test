4c425fb9f0ed2c2dade964796c3fa4fbf4ca2cce
Cam Poole <cameron.poole@fdbhealth.com>
2024-03-06 16:04:46 +0000

    SHould Fail

---
+++ test_fail.py
@@ -0,0 +1,4 @@
+azureToken="ILoveFDB123!"
+EbayPassword="ILoveFDB123!"
+password="ILoveFDB123!"
+connection="server=10.10.10.10;password=iawniuanf;"
---
+++ test_pass.py
@@ -0,0 +1,4 @@
+azureToken=client.GetToken("azureToken")
+EbayPassword=client.GetToken("Ebay")
+password=client.GetToken("pass")
+connection=client.GetToken("connString")
